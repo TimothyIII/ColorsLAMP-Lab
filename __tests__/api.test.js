eb9deda3d22f7ca7141fe9e0cc3a335393db2f8a
TimothyIII <136858294+TimothyIII@users.noreply.github.com>
2026-04-27 21:06:58 -0400

    Update api.test.js

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 test('PHP API returns valid color JSON', async () => {
   // Use localhost:8000 (we will set this up in GitHub Actions)
-  const response = await axios.get('http://localhost:8000/api/colors.php');
+  const response = await axios.get('http://localhost:8000/API/AddColor.php');
   expect(response.status).toBe(200);
   expect(typeof response.data).toBe('object');
 });
